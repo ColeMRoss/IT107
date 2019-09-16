b9acc55328a7a1d0baaefd16357621ea6324f7c4
ColeMRoss <54378629+ColeMRoss@users.noreply.github.com>
2019-09-16 11:00:47 -0400

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,19 @@
+alert ("click ok if you rate site 100 out of 10")
+
+var myName = "Dwayne Johnson";
+
+myName = "The Rock";
+
+var totalCost;
+var totalWithTax;
+
+var costPerItem = 2.50;
+var numberOfItems = 4;
+var taxRate = 0.06;
+ 
+totalCost = costPerItem * numberOfItems;
+totalWithTax = totalCost + (taxRate * totalCost);
+
+console.log(totalWithTax);
+
+console.log(myName)
