eedd6fb802b38200c1d52f277d4188dc9ceab38b
ColeMRoss <54378629+ColeMRoss@users.noreply.github.com>
2019-09-23 10:39:55 -0400

    Add files via upload

--- AAM.js
+++ AAM.js
@@ -9,14 +9,71 @@ var ColeObject = {
   var TempArray = [61, 79, 68, 71, 900];
   var months = ['Jan', 'March', 'April', 'June'];
   
+  var weatherObject = {
+    temperature: ["1000","100"],
+    conditions: ["cloudy", "sunny",],
+    sunriseTime: "6:00pm",
+    sunsetTime: "6:30pm",
+  }
+
+  var highTemperatures = [55, 57, 53, 47, 56, 50];
+  var lowTemperatures = [33, 30, 29, 35, 31, 32];
+
+
  
+  var todaysWeather = {
+    temperature: {
+      high: 55,
+      low: 32
+    },
+    conditions: "sunny",
+    astronomy: {
+      sunrise: "7:43 AM",
+      sunset: "5:09 PM"
+    }
+  }
+
+
+  
+
  
+  
  
   console.log(TempArray);
   console.log(months);
-  console.log(ColeObject);
+
+
+ 
+ 
   console.log ("My Favorite temperature Is " + TempArray[4] + " degrees");
-  
+  console.log(ColeObject.firstName);
+  console.log(ColeObject.lastName);
+  console.log(ColeObject.age);
+  console.log(ColeObject.likesTravel);
+  console.log(ColeObject.favoriteShows);
+  console.log(ColeObject.favoriteShows[0]);
+
+
   TempArray.push (1);
   months.splice(1, 0, 'Feb');
   months.splice(4, 1, 'May');
+
+
+  console.log(weatherObject);
+  console.log(weatherObject.temperature);
+  console.log(weatherObject.conditions);
+  console.log(weatherObject.sunriseTime);
+  console.log(weatherObject.sunsetTime);
+  console.log("It is currently " + weatherObject.conditions[1] + " and " + weatherObject.temperature[1] + " degrees");
+
+  console.log (highTemperatures[0]);
+  console.log (lowTemperatures[5]);
+  console.log(highTemperatures.reduce((a,b) => a + b, 0) / highTemperatures.length);
+  console.log (lowTemperatures.reduce((a,b) => a + b, 0) / lowTemperatures.length);
+
+
+  console.log (todaysWeather.temperature.high + " " + todaysWeather.conditions + " " + todaysWeather.astronomy.sunrise);
+ 
+
+
+  
