2835d279e8c41c77ac08fafa3232d0a4328c2c46
ColeMRoss <54378629+ColeMRoss@users.noreply.github.com>
2019-09-25 10:57:49 -0400

    Add files via upload

---
+++ Conditional.js
@@ -0,0 +1,113 @@
+var firstname = "cole";
+var favoriteFood = "Shrimp";
+var likestotravel = true;
+var fullsun = true;
+var cat = true;
+var likeDogs = true;
+
+
+
+
+
+if(likestotravel=true){
+    console.log("bon voyage!")
+} else {
+    console.log("Enjoy your couch!")
+}
+
+
+if (favoriteFood === "Shrimp"){
+    console.log("Congratulations! You have excellent taste!");
+  } else (console.log("Clearly you have not tried Shrimp"));
+
+  if (1>0){
+      console.log("True")}
+      else{
+          console.log("False")
+      }
+
+  
+
+
+
+    if(fullsun && cat){
+    console.log("MEooWWowW");
+    }
+
+
+   
+   //math
+   
+    if (1+1 > 5){ 
+        console.log("true fact")
+    }
+    else {
+        console.log("Lies")
+    }
+
+
+    if (4*5 <= 20){
+        console.log("true fact")
+    } 
+    else {
+        console.log ("Lies")
+    }
+
+    if (6-2 >= 0){
+        console.log("true fact")
+    }
+    else{
+        console.log ("Lies")
+    }
+
+
+    //Dog People vs. Cat People
+
+    if (likeDogs){
+        console.log("You're a dog person")
+    }
+    else{
+        console.log("You're a cat person")
+    }
+
+    //Generation Gaps
+
+    var birthYear = window.prompt("Birth Year?: ")
+
+    if (birthYear >= 1995 && birthYear <= 2012){
+        console.log ("Generation Z")
+    }
+    else if
+        (birthYear >= 1977 && birthYear <= 1994){
+        console.log("Generation Y")
+
+    }
+
+    //Student Grades
+    var grade = window.prompt("Grade?:")
+
+    if (grade >= 0 && grade <= 69){
+        console.log("Fantastic Grade");
+        window.alert("Fantastic Grade");
+    }
+    else if (grade >= 70 && grade <= 76){
+        console.log("Dilligent Grade");
+        window.alert("Dilligent Grade");
+    }
+    else if (grade >= 77 && grade <= 84){
+        console.log("Careful Grade");
+        window.alert("Careful Grade");
+    }
+    else if (grade >= 93 && grade <= 100){
+        console.log("Awful Grade");
+        window.alert("Awful Grade");
+    }
+
+
+
+
+    
+
+
+
+
