25ef4e837fb4f50035a6f98c5a01fde8ffadc48d
ColeMRoss <54378629+ColeMRoss@users.noreply.github.com>
2019-10-07 10:56:59 -0400

    Add files via upload

---
+++ Functions.js
@@ -0,0 +1,121 @@
+//variables
+var friendName = window.prompt("Friends Name");
+
+var greeting;
+if (timeOfDay === "morning") {
+    greeting = "Good morning";
+} else if (timeOfDay === "afternoon") {
+    greeting = "Good afternoon";
+} else if (timeOfDay === "evening") {
+    greeting = "Good evening";
+} else {
+    greeting = "Hello";
+}
+
+var timeOfDay = ("morning");
+
+
+var YourName = window.prompt("Your Name?");
+var destination = window.prompt("Dream Vacation Destination?");
+
+
+
+
+
+
+
+
+
+
+
+
+//functions
+function sayHelloWorld() {
+    console.log("Hello, world!");
+}
+
+
+function ilovejavascript() {
+    console.log("I love JavaScript")
+}
+
+function greetAFriend(friendName) {
+    console.log(`Hello, ${friendName}!`)
+}
+
+
+function greetAFriend(friendName, timeOfDay) {
+    var greeting;
+    if (timeOfDay === "morning") {
+        greeting = "Good morning";
+    } else if (timeOfDay === "afternoon") {
+        greeting = "Good afternoon";
+    } else if (timeOfDay === "evening") {
+        greeting = "Good evening";
+    } else {
+        greeting = "Hello";
+    }
+    var personalGreeting = `${greeting}, ${friendName}!`;
+    console.log(personalGreeting);
+}
+
+
+function Vacation(YourName, destination) {
+    var vacationlocation = YourName + " would love to visit " + destination; console.log(vacationlocation)
+}
+
+
+
+
+
+
+//tacotruck
+var shelltype = ("Hard");
+var topping = ("lettuce");
+function makeataco(shelltype, topping){ 
+var finishedtaco = ("Your " + shelltype + "shell taco with " + topping + " is ready!");
+console.log(finishedtaco)
+}
+
+
+
+
+
+
+//pizza
+
+
+
+
+
+
+
+
+//Calculator
+var numb1text = prompt("number1");
+var numb2text = prompt("number2");
+var numb1 = parseFloat(numb1text);
+var numb2 = parseFloat(numb2text);
+function add(numb1, numb2){
+var addition = (numb1+numb2);
+console.log(addition);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//
+sayHelloWorld();
+ilovejavascript();
+greetAFriend(friendName, timeOfDay);
+Vacation(YourName, destination);
+makeataco(shelltype, topping);
+add(numb1, numb2);
