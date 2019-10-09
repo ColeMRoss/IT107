9836c8f41cc389393e6c2eb1a89a0c54ed51577e
ColeMRoss <54378629+ColeMRoss@users.noreply.github.com>
2019-10-09 10:30:41 -0400

    Add files via upload

--- Calculator.js
+++ Calculator.js
@@ -1,37 +1,80 @@
 // addition
 
+var add1text = ("1");
+var add2text = ("3");
+var add1 = parseFloat(add1text);
+var add2 = parseFloat(add2text);
+
 function add(add1, add2){
     console.log("Answer:",add1+add2);
 }
 
-add(2,4);
+add(add1,add2);
+
+
+
+
+
 
 //Subtract
+
+var sub1text = ("4");
+var sub2text = ("6");
+var sub1 = parseFloat(sub1text);
+var sub2 = parseFloat(sub2text);
+
 
 function subtract(sub1, sub2){
     console.log("Answer:",sub1-sub2)
 }
 
-subtract(4,5);
+subtract(sub1,sub2);
+
+
+
 
 
 
 //multiply
 
+var mult1text = ("6");
+var mult2text = ("6");
+var mult1 = parseFloat(sub1text);
+var mult2 = parseFloat(sub2text
+
 function multiply(mult1, mult2){
     console.log("Answer:",mult1*mult2)
 }
 
-multiply(3,6);
+multiply(mult1,mult2);
+
+
+
+
 
 
 //divide
+
+var div1text = ("40");
+var div2text = ("6");
+var div1 = parseFloat(sub1text);
+var div2 = parseFloat(sub2text
 
 function divide(div1,div2){
     console.log("Answer:",div1/div2)
 }
 
-divide(40,8);
+divide(div1,div2);
+
+
+
+
+
+
+//
+
+
+
 
 
 
