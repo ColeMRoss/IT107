00ef06638addf42783cbea2489a2b105e2aeea0a
ColeMRoss <54378629+ColeMRoss@users.noreply.github.com>
2019-10-09 10:46:59 -0400

    Add files via upload

--- Calculator.js
+++ Calculator.js
@@ -1,12 +1,12 @@
 // addition
 
-var add1text = ("1");
-var add2text = ("3");
+var add1text = prompt("Add to number");
+var add2text = prompt("Add amount");
 var add1 = parseFloat(add1text);
 var add2 = parseFloat(add2text);
 
 function add(add1, add2){
-    console.log("Answer:",add1+add2);
+    console.log("Addition Answer:", add1 + " + " +  add2 + " = ", add1+add2);
 }
 
 add(add1,add2);
@@ -18,14 +18,14 @@ add(add1,add2);
 
 //Subtract
 
-var sub1text = ("4");
-var sub2text = ("6");
+var sub1text = prompt("Subtract from number");
+var sub2text = prompt("Subtract amount");
 var sub1 = parseFloat(sub1text);
 var sub2 = parseFloat(sub2text);
 
 
 function subtract(sub1, sub2){
-    console.log("Answer:",sub1-sub2)
+    console.log("Subtraction Answer:", sub1 + " - " +  sub2 + " = ",sub1-sub2)
 }
 
 subtract(sub1,sub2);
@@ -37,13 +37,13 @@ subtract(sub1,sub2);
 
 //multiply
 
-var mult1text = ("6");
-var mult2text = ("6");
-var mult1 = parseFloat(sub1text);
-var mult2 = parseFloat(sub2text);
+var mult1text = prompt("Multiply number");
+var mult2text = prompt("Multiply by");
+var mult1 = parseFloat(mult1text);
+var mult2 = parseFloat(mult2text);
 
 function multiply(mult1, mult2){
-    console.log("Answer:",mult1*mult2)
+    console.log("Multiplication Answer:", mult1 + " * " +  mult2 + " = ",mult1*mult2)
 }
 
 multiply(mult1,mult2);
@@ -55,13 +55,13 @@ multiply(mult1,mult2);
 
 //divide
 
-var div1text = ("40");
-var div2text = ("6");
-var div1 = parseFloat(sub1text);
-var div2 = parseFloat(sub2text);
+var div1text = prompt("Divide number");
+var div2text = prompt("Divide by");
+var div1 = parseFloat(div1text);
+var div2 = parseFloat(div2text);
 
 function divide(div1,div2){
-    console.log("Answer:",div1/div2)
+    console.log("Division Answer:", div1 + " / " +  div2 + " = ",div1/div2)
 }
 
 divide(div1,div2);
