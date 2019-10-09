4d44861b5be5aa280cc629a70594010c5b602180
ColeMRoss <54378629+ColeMRoss@users.noreply.github.com>
2019-10-09 09:51:53 -0400

    Add files via upload

---
+++ Loop.js
@@ -0,0 +1,36 @@
+//movies loop
+
+var movies = ["Shrek", "Shrek 2", "Shrek 3"];
+
+var i = 0;
+
+while(i < movies.length){
+  console.log(movies[i]);
+  i++;
+}
+
+
+
+
+
+//tweet loop
+
+var i = 0;
+
+while(i < 100){
+  console.log("TWEET TWEET!");
+  i++;
+}
+
+
+
+
+
+
+//foods loop
+
+var favoritefoods = ["Pizza", "Rice", "Spaghetti", "Garlic Bread"];
+
+for(var o = 0; o < favoritefoods.length; o++){
+    console.log(favoritefoods[o]);
+  }
