97ec73efd6c19d8bd8ae623778a0a7dc94128478
ColeMRoss <54378629+ColeMRoss@users.noreply.github.com>
2019-10-09 10:17:30 -0400

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,38 @@
+// addition
+
+function add(add1, add2){
+    console.log("Answer:",add1+add2);
+}
+
+add(2,4);
+
+//Subtract
+
+function subtract(sub1, sub2){
+    console.log("Answer:",sub1-sub2)
+}
+
+subtract(4,5);
+
+
+
+//multiply
+
+function multiply(mult1, mult2){
+    console.log("Answer:",mult1*mult2)
+}
+
+multiply(3,6);
+
+
+//divide
+
+function divide(div1,div2){
+    console.log("Answer:",div1/div2)
+}
+
+divide(40,8);
+
+
+
+
